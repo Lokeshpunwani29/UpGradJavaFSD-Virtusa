c64116022219cca93927ea0678d692d15dbed76b
Lokeshpunwani29 <lokeshpunwani29@gmail.com>
2025-12-10 01:36:51 +0530

    Local

--- Dec10/Eureka/AgentService/src/main/java/com/java/agentservice/repo/AgentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.java.agentservice.repo;
-
-import com.java.agentservice.model.Agent;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AgentRepository extends MongoRepository<Agent, String> {
-    Agent findByAgentId(int agentId);
-}
--- Dec10/Eureka/RegisterService/src/main/java/com/java/registerservice/repo/ComplaintRepo.java
+++
@@ -1,8 +0,0 @@
-package com.java.registerservice.repo;
-
-import com.java.registerservice.model.Complaint;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ComplaintRepo extends MongoRepository<Complaint, String> {
-    Complaint findByComplaintId(Integer complaintId);
-}
